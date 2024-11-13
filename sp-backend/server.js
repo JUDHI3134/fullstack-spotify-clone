d9c35d4b0f5c2ad800ea7669c1108d7f73c6facb
JUDHI3134 <judhistirbehera241@gmail.com>
2024-11-13 12:27:19 +0530

    album controller setup

--- sp-backend/server.js
+++ sp-backend/server.js
@@ -4,6 +4,7 @@ import "dotenv/config"
 import songRouter from "./src/routes/songRoute.js";
 import connectDB from "./src/config/mongodb.js";
 import connectCloudinary from "./src/config/cloudinary.js";
+import albumRouter from "./src/routes/albumRoute.js";
 
 //app config
 const app = express();
@@ -20,7 +21,8 @@ app.use(cors())
 app.get("/",(req,res)=> res.send("Api Working"));
 
 //initializing routes
-app.use("/api/song",songRouter)
+app.use("/api/song",songRouter);
+app.use("/api/album",albumRouter)
 
 
 app.listen(port,()=>{
---
+++ sp-backend/src/controllers/albumController.js
@@ -0,0 +1,14 @@
+import {v2 as cloudinary} from "cloudinary"
+import songModel from "../models/songModel.js";
+
+const addAlbum = async(req,res) =>{
+    
+}
+const listAlbum = async(req,res) =>{
+
+}
+const removeAlbum = async(req,res) =>{
+
+}
+
+export {addAlbum, listAlbum, removeAlbum}
